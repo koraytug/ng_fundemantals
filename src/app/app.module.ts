b5f5e6907e7eb9f34e2883fc76f088d6bb4e3303
Koray Tug <koray.tug@virtudev.net>
2020-11-11 21:40:27 +0300

    add eventDetails and add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToaStrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import {appRoutes} from './routes'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventService,ToaStrService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
